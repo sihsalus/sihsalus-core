753338a7801182061e8e79ee2618ece3efa2de53
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-05-21 20:33:26 +0000

    Strip trailing whitespace in AuthenticationUtilTest
    Co-authored-by: Codesmith <codesmith-bot@users.noreply.github.com>

--- sihsalus-module-authentication/src/test/java/org/openmrs/module/authentication/AuthenticationUtilTest.java
+++ sihsalus-module-authentication/src/test/java/org/openmrs/module/authentication/AuthenticationUtilTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class AuthenticationUtilTest {
-	
+
 	@Test
 	void getIntegerReturnsDefaultForInvalidNumber() {
 		assertEquals(7, AuthenticationUtil.getInteger("not-a-number", 7));
 	}
-	
+
 	@Test
 	void getIntegerReturnsParsedNumber() {
 		assertEquals(42, AuthenticationUtil.getInteger("42", 7));
