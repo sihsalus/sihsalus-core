77b0f1b1386ffd1a27d4ad90374b859e5e2864c4
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-05-21 15:31:34 -0500

    Handle invalid numeric settings

---
+++ sihsalus-module-authentication/src/test/java/org/openmrs/module/authentication/AuthenticationUtilTest.java
@@ -0,0 +1,18 @@
+package org.openmrs.module.authentication;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AuthenticationUtilTest {
+	
+	@Test
+	void getIntegerReturnsDefaultForInvalidNumber() {
+		assertEquals(7, AuthenticationUtil.getInteger("not-a-number", 7));
+	}
+	
+	@Test
+	void getIntegerReturnsParsedNumber() {
+		assertEquals(42, AuthenticationUtil.getInteger("42", 7));
+	}
+}
