444ccca4c78110d01413bec9a772e1439a981841
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-05-21 18:16:14 -0500

    Address remaining simple CodeQL quality findings

--- sihsalus-core-api/src/main/java/org/openmrs/util/databasechange/DuplicateLocationAttributeTypeNameChangeSet.java
+++ sihsalus-core-api/src/main/java/org/openmrs/util/databasechange/DuplicateLocationAttributeTypeNameChangeSet.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.openmrs.util.DatabaseUpdater;
 import org.openmrs.util.DatabaseUtil;
@@ -104,7 +105,7 @@ public class DuplicateLocationAttributeTypeNameChangeSet implements CustomTaskCh
 
 			for (Object o : duplicates.entrySet()) {
 				Map.Entry pairs = (Map.Entry) o;
-				HashSet<Integer> values = (HashSet<Integer>) pairs.getValue();
+				Set<Integer> values = (Set<Integer>) pairs.getValue();
 				List<Integer> duplicateNames = new ArrayList<Integer>(values);
 				int duplicateNameId = 1;
 				for (int i = 1; i < duplicateNames.size(); i++) {
