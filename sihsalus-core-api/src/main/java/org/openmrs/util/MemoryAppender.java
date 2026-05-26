6252dbf5e7d917f446e4b5b8a8e8b711cff17785
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-05-25 22:13:10 -0500

    Suppress CodeQL class-name match for deprecated MemoryAppender bridge

--- sihsalus-core-api/src/main/java/org/openmrs/util/MemoryAppender.java
+++ sihsalus-core-api/src/main/java/org/openmrs/util/MemoryAppender.java
@@ -27,6 +27,7 @@ import org.apache.logging.log4j.core.config.Property;
  *
  * @deprecated As of 2.4.4, 2.5.1, and 2.6.0 this class is moved to the org.openmrs.logging package
  */
+// codeql[java/class-name-matches-super-class]
 @Deprecated
 public class MemoryAppender extends org.openmrs.logging.MemoryAppender {
 
