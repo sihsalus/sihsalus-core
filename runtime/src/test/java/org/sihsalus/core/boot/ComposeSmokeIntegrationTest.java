2cdde09d31cb9199f8e6d220d0e4154dc2205cce
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2026-05-30 16:25:54 -0500

    Reemplazar TestRestTemplate por RestTemplate en ComposeSmokeIntegrationTest

--- runtime/src/test/java/org/sihsalus/core/boot/ComposeSmokeIntegrationTest.java
+++ runtime/src/test/java/org/sihsalus/core/boot/ComposeSmokeIntegrationTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -35,8 +35,7 @@ public class ComposeSmokeIntegrationTest {
     @LocalServerPort
     int port;
 
-    @Autowired
-    TestRestTemplate restTemplate;
+    RestTemplate restTemplate = new RestTemplate();
 
     @Test
     @Timeout(180)
